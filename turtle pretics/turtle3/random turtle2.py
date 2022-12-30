d7af1bedb3ed5c578ca0b5f7ecbed041534a6305
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2022-12-30 18:32:47 +0900

    Add files via upload

---
+++ turtle pretics/turtle3/random turtle2.py
@@ -0,0 +1,16 @@
+import turtle
+from random import*
+
+t=turtle.Turtle()
+t.shape('turtle')
+
+t.circle(randint(0,100))
+t.dot(randint(1,20))
+t.pensize(randint(1,10))
+t.setheading(randint(0,360))
+t.forward(randint(0,100))
+t.backward(randint(0,100))
+t.setx(randint(0,300))
+t.sety(randint(0,300))
+
+turtle.done()
