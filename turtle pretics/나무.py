5853a35910e76c52d4b5b0c6e854f8ccefb6106d
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-02-03 15:59:14 +0900

    Add files via upload

---
+++ turtle pretics/나무.py
@@ -0,0 +1,36 @@
+
+import turtle
+from random import*
+
+t=turtle.Turtle()
+t.color("#8c5d3f")
+t.left(90)
+t.forward(randint(100,150))
+t.left(randint(60,80))
+b=randint(50,120)
+t.forward(b)
+t.backward(b)
+t.right(randint(60,80))
+t.forward(randint(50,120))
+t.left(randint(60,80))
+b=randint(50,120)
+t.forward(b)
+t.backward(b)
+t.right(randint(60,80))
+t.forward(randint(50,120))
+t.backward(b)
+t.left(randint(60,80))
+b=randint(50,120)
+t.forward(b)
+t.backward(b)
+t.right(randint(60,80))
+t.forward(randint(50,120))
+t.penup()
+t.goto(0+100,200)
+t.color("#258a45")
+t.pendown()
+for i in range(100):
+    t.speed(100)
+    t.circle(i)
+    i=i-1
+turtle.done()
